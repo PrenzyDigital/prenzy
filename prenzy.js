64aff9fff28084ef3db36b13d815e8fbbf334026
PrenzyDigital <85508975+PrenzyDigital@users.noreply.github.com>
2021-06-08 20:01:41 -0300

    Delete prenzy.js

--- prenzy.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.getElementById("hamburger");
-const mainNav = document.getElementById("mainNav")
-
-hamburger.addEventListener("click", function () {
-
-  mainNav.classList.toggle("show");
-
-});
